9fc1651c83e2dd4ce1fbb2ddca70e6a95fc6d067
densyakun <densyakun1128@gmail.com>
2019-07-31 15:41:46 +0900

    mcmod.infoを更新。バージョンなどをmcmod.infoに表記

--- forge-1.12.2-14.23.2.2611-mdk/src/main/java/io/github/densyakun/soundblock/SoundBlock.java
+++ forge-1.12.2-14.23.2.2611-mdk/src/main/java/io/github/densyakun/soundblock/SoundBlock.java
@@ -6,11 +6,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = SoundBlock.MODID, name = SoundBlock.NAME, version = SoundBlock.VERSION)
+@Mod(modid = SoundBlock.MODID)
 public class SoundBlock {
 	public static final String MODID = "soundblock";
-	public static final String NAME = "SoundBlock";
-	public static final String VERSION = "1.0";
 
 	private static Logger logger;
 
